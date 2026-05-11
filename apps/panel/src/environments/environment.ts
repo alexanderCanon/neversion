a767d092a2210ee68a147c3fbc764b78031901a9
alexanderCanon <alexandercanon64@gmail.com>
2026-05-11 12:19:03 -0600

    feat: add production docker compose files and update platform catalog UI components

--- apps/panel/src/environments/environment.ts
+++ apps/panel/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://api.neversion.com',
+  grafanaUrl: '',
   supabaseUrl: 'https://ujadaoctohwplnhiwgrm.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqYWRhb2N0b2h3cGxuaGl3Z3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MDg5MjAsImV4cCI6MjA4MTQ4NDkyMH0.EwOPfi-fp-hFkEZZhGD4adzwpNPzn25ZPoN4HAECaNEanon-key-placeholder'
 };
