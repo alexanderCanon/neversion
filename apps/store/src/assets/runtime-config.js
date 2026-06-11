ed745fc15d3316eafa589f8756da27f4ddc54de4
alexanderCanon <alexandercanon64@gmail.com>
2026-06-10 18:13:37 -0600

    changes

--- apps/store/src/assets/runtime-config.js
+++ apps/store/src/assets/runtime-config.js
@@ -1,6 +1,6 @@
 window.__NEVERSION_CONFIG__ = {
-  apiUrl: 'https://api.neversion.com',
+  apiUrl: 'http://localhost:8080',
   supabaseUrl: 'https://ujadaoctohwplnhiwgrm.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqYWRhb2N0b2h3cGxuaGl3Z3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MDg5MjAsImV4cCI6MjA4MTQ4NDkyMH0.EwOPfi-fp-hFkEZZhGD4adzwpNPzn25ZPoN4HAECaNE',
-  storeVendorUuid: '4c78e18d-d9d6-4707-a39a-a298dc71c103'
+  storeVendorUuid: '97e5cc13-ff12-473e-9da2-fa4308edbce6'
 };
